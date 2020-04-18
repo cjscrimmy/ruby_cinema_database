81573b57d42a2d9be4b9b3fbeff3e1e4baf17692
cjscrimmy <craigscrimgeour@yahoo.com>
2020-04-18 11:28:31 +0100

    add method to customer class that shows all the movies a customer is going to see

--- models/customer.rb
+++ models/customer.rb
@@ -50,6 +50,16 @@ class Customer
         SqlRunner.run(sql, values)
     end
 
+    def films()
+        sql = "SELECT films.* FROM films
+        INNER JOIN tickets
+        ON films.id = tickets.film_id
+        WHERE tickets.customer_id = $1"
+        values = [@id]
+        film_data = SqlRunner.run(sql, values)
+        return Film.map_items(film_data)
+    end
+    
     def self.delete_all()
         sql = "DELETE FROM customers"
         SqlRunner.run(sql)
