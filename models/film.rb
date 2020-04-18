2f00e79bfc52e91c7526f8821ff842ba1f130c73
cjscrimmy <craigscrimgeour@yahoo.com>
2020-04-18 10:42:54 +0100

    add update, delete and delete_all methods for film class

--- models/film.rb
+++ models/film.rb
@@ -25,4 +25,31 @@ class Film
         films = SqlRunner.run(sql, values).first()
         @id = films['id'].to_i
     end
+
+    def update()
+        sql = "UPDATE films SET
+        (
+            title, 
+            price
+        )
+        =
+        (
+            $1, $2
+        )
+        WHERE id = $3"
+        values = [@title, @price, @id]
+        SqlRunner.run(sql, values)
+    end
+
+    def delete()
+        sql = " DELETE FROM films
+        WHERE id = $1"
+        values =[@id]
+        SqlRunner.run(sql, values)
+    end
+
+    def self.delete_all()
+        sql = "DELETE FROM films"
+        SqlRunner.run(sql)
+    end
 end
